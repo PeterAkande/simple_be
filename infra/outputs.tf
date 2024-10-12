616fff4560f0eb049d5819f641aae0501ed2e1bf
peterakande <akandepeter86@gmail.com>
2024-10-12 19:13:08 +0100

    chore: update workflow and outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -16,3 +16,29 @@ output "redis_endpoint" {
   description = "Redis endpoint"
   value       = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
+
+output "postgres_port" {
+  description = "The port PostgreSQL is listening on."
+  value       = aws_db_instance.postgres.port
+}
+
+output "postgres_user" {
+  description = "The username for the PostgreSQL instance."
+  value       = aws_db_instance.postgres.username
+}
+
+output "postgres_password" {
+  description = "The password for the PostgreSQL instance."
+  value       = aws_db_instance.postgres.password
+  sensitive   = true # Marks the output as sensitive to hide it in logs.
+}
+
+output "redis_endpoint" {
+  description = "The endpoint of the Redis instance."
+  value       = aws_elasticache_cluster.redis.configuration_endpoint
+}
+
+output "redis_port" {
+  description = "The port Redis is listening on."
+  value       = aws_elasticache_cluster.redis.port
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,8 @@
-variable "aws_profile" {
-  description = "AWS profile to use"
-  type        = string
+# variable "aws_profile" {
+#   description = "AWS profile to use"
+#   type        = string
 
-}
+# }
 
 variable "aws_region" {
   description = "AWS region to use"
