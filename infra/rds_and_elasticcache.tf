a0b84f7779dc77bbc156b4b04b9bd0c50584c258
peterakande <akandepeter86@gmail.com>
2024-10-12 21:18:53 +0100

    chore: update workflow and outputs

--- infra/rds_and_elasticcache.tf
+++ infra/rds_and_elasticcache.tf
@@ -17,7 +17,6 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis7.cluster.on"
+  parameter_group_name = "default.redis7"
   port                 = 6379
-
 }
