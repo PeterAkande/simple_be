64e5e9c5f90e6ed5f3174118a9144d7bb77ed6ee
peterakande <akandepeter86@gmail.com>
2024-10-13 06:17:48 +0100

    chore: fix working dictory for docker build

--- infra/iam.tf
+++ infra/iam.tf
@@ -26,3 +26,39 @@ resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attach
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+
+resource "aws_iam_policy" "cloudwatch_policy" {
+  name        = "CloudWatchAccessPolicy"
+  description = "Policy for CloudWatch access"
+
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "events:PutEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
+  policy_arn = aws_iam_policy.cloudwatch_policy.arn
+  role       = aws_iam_role.ecs_task_execution.name
+}
